f8456de8c03e0468d1d7021cdbdb2584fa449f72
dannrs <danniramdhani@proton.me>
2024-05-18 23:43:43 +0700

    add some packages and update flake

--- home/modules/default.nix
+++ home/modules/default.nix
@@ -3,12 +3,12 @@
     ./packages.nix
     ./fish.nix
     ./flatpak.nix
+    ./gtk-qt.nix
     ./hyprland.nix
     ./mako.nix
     ./rofi.nix
     ./starship.nix
     ./text-editor.nix
-    ./theming.nix
     ./waybar.nix
     ./wezterm.nix
   ];
